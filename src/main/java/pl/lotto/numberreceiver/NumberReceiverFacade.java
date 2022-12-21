9b6535e86e8aec50088ac2cf1b552b7147272990
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2022-12-21 11:02:41 +0100

    implementation

--- src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/numberreceiver/NumberReceiverFacade.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 public class NumberReceiverFacade {
 
+    final String CORRECT_RESULT_MESSAGE = "correct";
+    final String FAILED_RESULT_MESSAGE = "failed";
+    final int MAX_NUMBERS_FROM_USER = 6;
+
     NumberReceiverResultDto inputNumbers(List<Integer> numbersFromUser) {
-        return new NumberReceiverResultDto("correct");
+        if (isSixNumbersFromUser(numbersFromUser)) {
+            return new NumberReceiverResultDto(CORRECT_RESULT_MESSAGE);
+        } else {
+            return new NumberReceiverResultDto(FAILED_RESULT_MESSAGE);
+        }
+    }
+
+    private boolean isSixNumbersFromUser(List<Integer> numbersFromUser) {
+        return numbersFromUser.size() == MAX_NUMBERS_FROM_USER;
     }
 }
